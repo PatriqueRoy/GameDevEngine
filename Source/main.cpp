e1f28cadabd6a8259951f8a9452e4331735bc1c0
Patrique Roy <patman333@live.com>
2018-02-06 12:54:03 -0500

    Initial Commit

---
+++ Source/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <Windows.h>
+#include <direct.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <SFML/Graphics.hpp>
+
+
+int main() {
+	sf::RenderWindow window(sf::VideoMode(800, 600),
+		"Hello SFML", sf::Style::Default);
+
+	sf::Font font;
+	font.loadFromFile("C:/Windows/Fonts/Arial.ttf");
+
+	sf::Text text;
+	text.setFont(font);
+	text.setPosition(200, 200);
+	text.setString("Hello SFML");
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+		window.clear();
+		window.draw(text);
+		window.display();
+	}
+	return 0;
+}
