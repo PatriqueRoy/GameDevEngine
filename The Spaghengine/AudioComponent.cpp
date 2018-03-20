a5d2a2521fb8288091a9ed5d9249c6f17db5ef9f
Nicholas Bulfon <pendragon177@hotmail.com>
2018-03-19 20:10:56 -0400

    audio

--- The Spaghengine/AudioComponent.cpp
+++ The Spaghengine/AudioComponent.cpp
@@ -1,13 +1,19 @@
 #include "AudioComponent.h"
 
+
 void  AudioComponent::Awake()
 {
 	sf::SoundBuffer buffer;
 	if (!buffer.loadFromFile("sound.wav"))
 	{
-		//Error
+		return;
 	}
 
+	sf::Music music;
+	if (!music.openFromFile("sound.wav"))
+	{
+		return;
+	}
 }
 
 
@@ -16,7 +22,39 @@ void  AudioComponent::Update()
 	sf::SoundBuffer buffer;
 	if (!buffer.loadFromFile("sound.wav"))
 	{
-		//Error
+		return;
 	}
+
+	sf::Music music;
+	if (!music.openFromFile("sound.wav"))
+	{
+		return;
+	}
+}
+
+void playSound(const std::string& filename)
+{
+	// Load a sound buffer from a wav file
+	sf::SoundBuffer buffer;
+	if (!buffer.loadFromFile("resources/" + filename))
+		return;
+
+	// Create a sound instance and play it
+	sf::Sound sound(buffer);
+	sound.play();
+
+}
+
+
+////////////////////////////////////////////////////////////
+/// Play a music
+///
+////////////////////////////////////////////////////////////
+void playMusic(const std::string& filename)
+{
+	// Load an ogg music file
+	sf::Music music;
+	if (!music.openFromFile("resources/" + filename))
+		return;
 
 }
--- The Spaghengine/AudioComponent.h
+++ The Spaghengine/AudioComponent.h
@@ -1,6 +1,9 @@
 #pragma once
 #include "..\Source\BaseComponent.h"
 #include <SFML\Audio.hpp>
+#include <iostream>
+#include <string>
+
 
 class AudioComponent : public BaseComponent {
 public:
@@ -8,6 +11,12 @@ public:
 	void Start();
 	void Update();
 	void LateUpdate();
+
+	void playSound(const std::string& filename);
+	void playMusic(const std::string& filename);
+
+	
+	std::string& filename;
 
 	sf::SoundBuffer buffer;
 	sf::Sound sound;
